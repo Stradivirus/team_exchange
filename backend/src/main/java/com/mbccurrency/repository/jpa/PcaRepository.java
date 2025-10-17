2a5affa88802b07bf01c52d62a5e52f6e22ee7d2
Super User <root@DESKTOP-4G7I13F.localdomain>
2025-10-17 19:13:12 +0900

    자잘한 수정

--- backend/src/main/java/com/mbccurrency/repository/jpa/PcaRepository.java
+++
@@ -1,8 +0,0 @@
-// repository/PcaRepository.java
-package com.mbccurrency.repository.jpa;
-
-import com.mbccurrency.entity.Pca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.time.LocalDate;
-
-public interface PcaRepository extends JpaRepository<Pca, LocalDate> {}
